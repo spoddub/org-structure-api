bee6d183edf98f5f9a551951f858515a295daa80
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2026-05-21 12:51:07 +0300

    add go models

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,1 +1,21 @@
 package model
+
+import "time"
+
+type Department struct {
+	ID        int64     `gorm:"primaryKey" json:"id"`
+	Name      string    `json:"name"`
+	ParentID  *int64    `json:"parent_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Employee struct {
+	ID           int64      `gorm:"primaryKey" json:"id"`
+	DepartmentID int64      `json:"department_id"`
+	FullName     string     `json:"full_name"`
+	Position     string     `json:"position"`
+	HiredAt      *time.Time `json:"hired_at"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
+}
