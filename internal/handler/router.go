1ce83efb657034da628ee5cdb23f663ced8eb3ef
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2026-05-21 17:00:38 +0300

    fix typo

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -5,8 +5,8 @@ import "net/http"
 func NewRouter(h *Handler) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST/departments/", h.createDepartment)
-	mux.HandleFunc("POST/departments/{id}/employees", h.createEmployee)
+	mux.HandleFunc("POST /departments/", h.createDepartment)
+	mux.HandleFunc("POST /departments/{id}/employees/", h.createEmployee)
 
 	return mux
 }
